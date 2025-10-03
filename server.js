64de8dfc1c08c4aae2e114b5e961b34e41d078da
Girija-Kangutkar <kangutkargirija075@gmail.com>
2025-10-03 19:11:40 +0530

    added get router

--- server.js
+++ server.js
@@ -9,6 +9,14 @@ app.use(cors());
 
 app.use("/app", productRouter);
 
+app.get("/", (req, res) => {
+  try {
+    res.status(200).json({ msg: "Everything is working fine" });
+  } catch (error) {
+    res.status(500).json({ msg: "Something went wrong" });
+  }
+});
+
 app.listen(3000, () => {
   console.log("Server is running on port 30000");
 });
